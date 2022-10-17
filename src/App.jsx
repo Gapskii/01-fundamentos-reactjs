3ebff1009f4cd1dc4075a69ac5eb3f35036ede88
mateusgapski <mateusgapski@hubii.com.br>
2022-10-17 09:58:46 -0300

    feat/changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,22 @@ import styles from './App.module.css';
 
 import './global.css'
 
+const posts = [
+  {
+    id: 1,
+    author: [
+      avatarUrl: 'https://github.com/gapskii.png',
+      name: 'Mateus Gapski',
+      role: 'Web Developer'
+    ]
+  },
+  content: [
+    { type: 'paragraph', content: 'Fala galeraa 👋', },
+    { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀', },
+    { type: 'link', content: 'jane.design/doctorcare', },
+  ],
+];
+
 export function App() {
   return (
     <div>
